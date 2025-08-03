f6b8db826b7124de34c95cc17a8c0c37e2057529
Vitaly Govor <vitaly.govor@icloud.com>
2025-08-03 20:38:51 +0300

    $derived

---
+++ src/routes/$derived/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+class Unit {
+    value = $state(0)
+
+    constructor(name) {
+        this.name = $state(name)
+    }
+
+    increase() {
+        this.value += 1
+    }
+
+    decrease() {
+        this.value -= 1
+    }
+}
+
+let a = new Unit("a")
+let b = new Unit("b")
+
+let sum = $derived(a.value + b.value)
+let complexSum = $derived.by(() => {
+    return sum * sum
+})
+</script>
+
+<button onclick="{() => a.decrease()}">-</button>
+<span>a: {a.value}</span>
+<button onclick="{() => a.increase()}">+</button>
+
+<button onclick="{() => b.decrease()}">-</button>
+<span>a: {b.value}</span>
+<button onclick="{() => b.increase()}">+</button>
+
+<br>
+
+<span>Sum: {sum}</span>
+<span>complexSum: {complexSum}</span>
