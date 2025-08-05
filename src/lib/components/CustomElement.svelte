862ef0feb979089c5a6fd94aedf45ad849be82f9
vitalyozza <vitaly.govor@icloud.com>
2025-08-05 11:10:34 +0300

    $host

---
+++ src/lib/components/CustomElement.svelte
@@ -0,0 +1,10 @@
+<svelte:options customElement="my-stepper"/>
+
+<script lang="ts">
+    function dispatch(type) {
+        $host().dispatchEvent(new CustomEvent(type))
+    }
+</script>
+
+<button onclick={() => dispatch('decrement')}>decrement</button>
+<button onclick={() => dispatch('increment')}>increment</button>
---
+++ src/routes/$host/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '$lib/components/CustomElement.svelte';
+
+	let count = $state(0);
+</script>
+
+<my-stepper 
+    ondecrement={() => (count -= 1)} 
+    onincrement={() => (count += 1)}
+></my-stepper>
+
+<p>Count: {count}</p>
