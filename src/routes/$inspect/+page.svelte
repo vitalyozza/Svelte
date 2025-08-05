bf83de95a880698de02de7b6478834b034d736dd
vitalyozza <vitaly.govor@icloud.com>
2025-08-05 10:59:03 +0300

    $inspect

---
+++ src/routes/$inspect/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    let name = $state("Vitaly")
+
+    // $inspect(name)
+
+    $inspect(name).with((type, name) => {
+        console.log(type, name, `(${name.length})`)
+    })
+</script>
+
+<label for="name">
+    Name: <input type="text" id="name" bind:value={name}>
+</label>
