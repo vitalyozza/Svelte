2d78a0f20221c9b6efb81049c353c9d4315fe0af
vitalyozza <vitaly.govor@icloud.com>
2025-08-05 10:52:43 +0300

    $bindable

---
+++ src/lib/components/BindableComponent.svelte
@@ -0,0 +1,7 @@
+<script>
+    let { name = $bindable() } = $props()
+</script>
+
+<label for="componentName">
+    Component Name <input type="text" bind:value={name}>
+</label>
---
+++ src/routes/$bindable/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import BindableComponent from "$lib/components/BindableComponent.svelte";
+
+    let name = $state("Vitaly")
+</script>
+
+<h3>Example with reactive inputs</h3>
+
+<label for="name">
+    Name
+    <input type="text" id="name" bind:value={name}>
+</label>
+<br>
+<BindableComponent bind:name={name}/>
