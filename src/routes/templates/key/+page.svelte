726d1ee3d02d97d9d8fb78b823ae1b573a80a9ae
vitalyozza <vitaly.govor@icloud.com>
2025-08-07 13:06:38 +0300

    Added using keys

---
+++ src/routes/templates/key/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { slide, fade } from 'svelte/transition';
+
+	let count = $state(0);
+	let countShow = $state(true);
+
+	let increase = () => {
+		count += 1;
+	};
+
+	let decrease = () => {
+		count -= 1;
+	};
+</script>
+
+{#if countShow}
+	<span transition:fade>
+		<button onclick={decrease}>-</button>
+		{#key count}
+			<div style="width: 18px; text-align: center;" transition:slide>{count}</div>
+		{/key}
+		<button onclick={increase}>+</button>
+	</span>
+{/if}
+<br />
+<label for="countShow">
+	<input type="checkbox" name="countShow" id="countShow" bind:checked={countShow} /> show count
+</label>
